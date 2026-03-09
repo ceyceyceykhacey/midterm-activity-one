24b8e5f67468816d9afc522edf0d8c1b73e4d945
lictao_k <lictao_k@ca01.ub.lan>
2026-03-09 12:01:54 +0800

    Home page.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,20 @@ import './App.css'
 import {Routes, Route} from 'react-router';
 import Home from './pages/Home';
 
+function App() {
+  return (
+    <div>
+      <Navigation />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
 
 
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+function Home() {
+return (
+    <div>
+<h2>Welcome to Student Dashboard</h2>
+<p>This app demonstrates React Router, useState, and useEffect.</p>
+</div>
+);
+
+}
+
+export default Home
